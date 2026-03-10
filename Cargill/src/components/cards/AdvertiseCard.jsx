9175e39f4799760f8579ec1298109880d782672b
ILikeCherryJam <ilikejammuch@gmail.com>
2026-03-10 11:43:18 +0500

    a

--- Cargill/src/components/cards/AdvertiseCard.jsx
+++ Cargill/src/components/cards/AdvertiseCard.jsx
@@ -3,10 +3,24 @@ import ReadButton from "./ReadButton";
 
 export default function AdvertiseCard(props) {
   return (
-    <div className="advertise-card">
-        <h1 className="advertise-title advertise-el">{props.title}</h1>
-        <p className="advertise-description advertise-el">{props.description}</p>
-        <ReadButton className="advertise-el" text={props.readBtn} />
+    <div 
+      className="advertise-card"
+      style={{backgroundColor: props.backColor}}
+      >
+        <h1 className="advertise-title advertise-el"
+          style={{color: props.textColor}}
+        >
+          {props.title}
+        </h1>
+        <p className="advertise-description advertise-el"
+          style={{color: props.textColor}}
+        >
+          {props.description}
+        </p>
+        <ReadButton className="advertise-el" 
+          text={props.readBtn} 
+          link={props.btnLink}  
+        />
     </div>
   );
 }
--- Cargill/src/components/mainPage/adContainer.jsx
+++ Cargill/src/components/mainPage/adContainer.jsx
@@ -9,16 +9,23 @@ export default function AdContainer() {
                     title="People"
                     description="With 155K+ colleagues in 70 countries, we work as a global network to bring together the people, ideas and resources that can nourish lives around the world.​"
                     readBtn="Contacts"
+                    backColor="#012912"
+                    textColor="#BDE588"
+                    btnLink="../../about   "
                 />
                 <AdvertiseCard 
                     title="Products"
                     description="Partnering across the supply chain, we bring food, ingredients, agricultural solutions and industrial products to meet customers’ ever-evolving needs.​"
                     readBtn="Products & Services"
+                    backColor="#BDE588"
+                    textColor="#012912"
                 />
                 <AdvertiseCard 
                     title="News"
                     description="With operations in 70 countries, Cargill and our customers are making news around the globe. Here are some of the latest stories."
                     readBtn="More News"
+                    backColor="#fff"
+                    textColor="#398245"
                 />
             </div>
         </div>
