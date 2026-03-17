3f3eed014b1fb8b0fac8457e083aff15bc3c9f59
ILikeCherryJam <ilikejammuch@gmail.com>
2026-03-17 11:40:30 +0500

    contacts setup

--- Cargill/src/components/mainPage/Footer.jsx
+++ Cargill/src/components/mainPage/Footer.jsx
@@ -1,10 +1,33 @@
 import './css/footer.css';
+import YoutubeLogo from "../../assets/logos/youtube.png";
+import InstagramLogo from "../../assets/logos/inst.png";
+import LinkedInLogo from "../../assets/logos/linked.png";
+import FacebookLogo from "../../assets/logos/face.jfif";
 
 export default function Footer() {
     return (
         <section className="footer">
             <div className="footer-content">
-                <p>aaaaa</p>
+                <div className="contact-container">
+                    <a href="" className="contact-btn">
+                        <img src={YoutubeLogo} alt="logo" />
+                    </a>
+                    <a href="" className="contact-btn">
+                        <img src={InstagramLogo} alt="logo" />
+                    </a>
+                    <a href="" className="contact-btn">
+                        <img src={LinkedInLogo} alt="logo" />
+                    </a>
+                    <a href="" className="contact-btn">
+                        <img src={FacebookLogo} alt="logo" />
+                    </a>
+                </div>
+                <a href="" className="contact">
+
+                </a>
+                <p className="footer-cargill">
+
+                </p>
             </div>
         </section>
     );
